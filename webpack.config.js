e00ebce3765af5f709083d62f65f9afb2fcbff79
incuisting <xuyanting9634@163.com>
2017-08-29 14:09:04 +0800

    播放器页面布局

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,40 @@
 const path = require('path');
 
 module.exports = {
-  entry: {
-    index:'./src/js/index.js',
-    songs:'./src/js/songs.js'
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module:{
-      rules:[
-          {
-              test:/\.css$/,
-              use:[
-                  'style-loader',
-                  'css-loader',
-                  'postcss-loader'
-              ]
-          },
-          {
-            test: /\.scss$/,
-            use: [{
-                loader: "style-loader"
-            }, {
-                loader: "css-loader"
-            },{
-                loader: "postcss-loader"
+    entry: {
+        index: './src/js/index.js',
+        songs: './src/js/songs.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'postcss-loader'
+                ]
             },
-             {
-                loader: "sass-loader",
-                options: {
-                    includePaths: ["absolute/path/a", "absolute/path/b"]
-                }
-            }]
-        }
-      ]
-  }
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: "style-loader"
+                }, {
+                    loader: "css-loader"
+                }, {
+                    loader: "postcss-loader"
+                },
+                {
+                    loader: "sass-loader",
+                    options: {
+                        includePaths: ["absolute/path/a", "absolute/path/b"]
+                    }
+                }]
+            }
+        ]
+    }
 };
