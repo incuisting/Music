4f1430e41c4142e50f81f5cb9ab28c9c7e08966f
incuisting <xuyanting9634@163.com>
2017-08-30 14:40:59 +0800

    最新音乐列表渲染的js

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,28 @@ import 'normalize.css'
 import '../css/reset.css'
 import '../css/index.scss';
 
-console.log('aa');
+import $ from 'jquery';
+
+$(function () {
+    $.get('../../server/songs.json').then(function (response) {
+        let items = response
+        items.forEach(i => {
+            let $li = $(
+                `
+                <li>
+                <a href="./song.html?id=${i.id}">
+                <h3>${i.name}</h3>
+                <p>演唱者-专辑</p>
+                <svg class="play">
+                <use xlink:href="#icon-play-circled"></use>
+                </svg>
+                </a>
+                </li>
+                `
+            )
+            $('#lastsetMusic').append($li)
+        })
+        $('#lastestMusicLoading').remove()
+    })
+})
+
